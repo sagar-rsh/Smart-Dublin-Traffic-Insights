d8ce3b1737193b2835b8a7208d6156a2d01d4713
Sagar Poojary <55018887+sagar-rsh@users.noreply.github.com>
2025-04-08 20:09:57 +0000

    Updated credentials path

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                   = var.aws_region
-  shared_credentials_files = ["./.aws/credentials"]
+  shared_credentials_files = ["../.aws/credentials"]
   profile                  = var.aws_profile
 }
