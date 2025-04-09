1d586cbe5fed3897bf7aaef8e2f9da65dd92ebef
Sagar Poojary <55018887+sagar-rsh@users.noreply.github.com>
2025-04-09 15:55:27 +0000

    Updated security group for dbt cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,46 @@ resource "aws_redshiftserverless_namespace" "this" {
   iam_roles           = [aws_iam_role.redshift_s3_access.arn]
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create a security group allowing inbound access from dbt Cloud IPs
+resource "aws_security_group" "redshift_dbt_cloud_access" {
+  name        = "redshift-dbt-cloud-access"
+  description = "Allow dbt Cloud IPs to access Redshift"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "Redshift Dbt Cloud Access"
+  }
+}
+
+# Inbound rules for each dbt Cloud IP address (Redshift port 5439)
+resource "aws_security_group_rule" "dbt_inbound_rules" {
+  for_each = toset([
+    "52.45.144.63",
+    "54.81.134.249",
+    "52.22.161.231",
+    "52.3.77.232",
+    "3.214.191.130",
+    "34.233.79.135"
+  ])
+
+  type              = "ingress"
+  from_port         = 5439
+  to_port           = 5439
+  protocol          = "tcp"
+  cidr_blocks       = [format("%s/32", each.key)]
+  security_group_id = aws_security_group.redshift_dbt_cloud_access.id
+}
+
+# Attach the security group to the Redshift Serverless workgroup
 resource "aws_redshiftserverless_workgroup" "this" {
   workgroup_name      = var.redshift_workgroup
   namespace_name      = aws_redshiftserverless_namespace.this.namespace_name
   publicly_accessible = true
+
+  # Attach security group
+  security_group_ids = [aws_security_group.redshift_dbt_cloud_access.id]
 }
